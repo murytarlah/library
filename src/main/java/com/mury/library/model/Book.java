6e13066074c549803da85276835c20e75b553bb5
murytarlah <murytarlah.dev@gmail.com>
2023-12-05 08:49:06 +0100

    removed unused import

--- src/main/java/com/mury/library/model/Book.java
+++ src/main/java/com/mury/library/model/Book.java
@@ -4,7 +4,6 @@ import java.util.UUID;
 
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 
